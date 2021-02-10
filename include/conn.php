cf481f2246ec66e9701509dd175574503ba66119
Grapefy <grapefy.tecnologia@gmail.com>
2021-02-09 22:34:12 -0300

    BD FA

--- include/conn.php
+++ include/conn.php
@@ -1,10 +1,10 @@
 <?php
 
 
-$servername = "localhost";
-$database = "bd_fa";
-$username = "root";
-$senhaserver = "";
+$servername = "br78.hostgator.com.br";
+$database = "felic009_bd_fa";
+$username = "felic009_lks";
+$senhaserver = "Oigalera123";
 // Create connection
 $conn = mysqli_connect($servername, $username, $senhaserver, $database);
 // Check connection
